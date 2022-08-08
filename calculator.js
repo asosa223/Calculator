ca8bb6e33fcf7b15074ce9ab327c6c2ef7711821
Alec Sosa <alecsosa223@gmail.com>
2022-08-08 12:53:50 -0500

    Added multiple operator functions

--- calculator.js
+++ calculator.js
@@ -1,15 +1,52 @@
-function add() {
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    }
+    else if (operator === '-') {
+        return subtract(a, b);
+    }
+    else if (operator === '*') {
+        return multiply(a, b);
+    }
+    else if (operator === '÷') {
+        return divide(a, b);
+    }
+    else {
+        return alert('ERROR');
+    }
+}
+
+function displayOperand() {
+    const currentOperand = document.querySelector('.current-operand');
+    const number = document.querySelectorAll('#number');
+
+    number.forEach(num => {
+        num.addEventListener('click', function(){
+            currentOperand.innerText += this.innerText.toString();   
+        });
+        if(num === '.' && this.currentOperand.innerText.includes('.')) return; //Only display one period
+        
+    });
+}
+
+function displayTotal() {
 
 }
 
-function subtract() {
-    
-}
-
-function multiply() {
-    
-}
-
-function divide() {
-    
-}
+displayOperand();
