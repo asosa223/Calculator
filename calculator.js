384fa914331f218b993b148faf4a88a6cd46580c
Alec Sosa <alecsosa223@gmail.com>
2022-08-10 15:44:30 -0500

    Display now shows number and operator inputs

--- calculator.js
+++ calculator.js
@@ -34,14 +34,46 @@ function operate(operator, a, b) {
 
 function displayOperand() {
     const currentOperand = document.querySelector('.current-operand');
+    const previousOperand = document.querySelector('.previous-operand');
+    const equals = document.querySelector('#equals');
     const number = document.querySelectorAll('#number');
+    const operator = document.querySelectorAll('#operator')
 
     number.forEach(num => {
-        num.addEventListener('click', function(){
+        num.addEventListener('click', () => {
             if(num.textContent === '.' && currentOperand.textContent.includes('.')) return; 
             currentOperand.textContent += num.textContent;
         });
     });
+
+    operator.forEach(op => {
+        op.addEventListener('click', () => {
+            if (op.textContent === '+') {
+                previousOperand.textContent = `${currentOperand.textContent} +`;
+                currentOperand.textContent = '';
+            }
+            else if (op.textContent === '-'){
+                previousOperand.textContent = `${currentOperand.textContent} -`;
+                currentOperand.textContent = '';
+            }
+            else if (op.textContent === '*'){
+                previousOperand.textContent = `${currentOperand.textContent} *`;
+                currentOperand.textContent = '';
+            }
+            else if (op.textContent === '÷'){
+                previousOperand.textContent = `${currentOperand.textContent} ÷`;
+                currentOperand.textContent = '';
+            }
+        });
+    });
+
+    
+    equals.addEventListener('click', () => {
+        previousOperand.textContent += ` ${currentOperand.textContent}`;
+        let total = (operator, parseInt(previousOperand.textContent), parseInt(currentOperand.textContent));
+    });
+   
+
 }
 
 function updateDisplay() {
@@ -50,9 +82,9 @@ function updateDisplay() {
 
 displayOperand();
 
-//When number is pressed, it is shown on display
+//When number is pressed, it is shown on display ✅
 //when del is pressed, remove most recent number pressed
-//when operator is pressed, number on display is sent to the top along with operator
+//when operator is pressed, number on display is sent to the top along with operator ✅
 //enter another number and when equals is pressed, number is sent to top and calculation is made
 //calculation should now show on main display
 //calculation number is stored and user can repeat previous steps
